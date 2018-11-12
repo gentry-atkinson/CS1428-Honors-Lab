5838b9e2f2a81e544b9bdd72f1ef7e3f7bc862ef
Atkinson <gma23@txstate.edu>
2018-11-12 13:35:53 -0600

    added lab 11 py

---
+++ atkinson_lab11.py
@@ -0,0 +1,10 @@
+#! python
+
+phrase = "This is lab 11."
+print(phrase)
+
+myList = {1, 2, 3, 'a', 'b', 'c', 1.1, 2.2, 3.3}
+	
+for value in myList:				#iterate through myList
+	if (isinstance(value, int)):	#check if value is int
+		print(value)				#print value
