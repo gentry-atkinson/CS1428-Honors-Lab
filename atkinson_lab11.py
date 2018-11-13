66fc10479a55351e63353fd24df6646b0a472f01
Gentry Atkinson <gma23@txstate.edu>
2018-11-13 11:41:18 -0600

    added links to pdf. changed py to list rather than set

--- atkinson_lab11.py
+++ atkinson_lab11.py
@@ -3,7 +3,7 @@
 phrase = "This is lab 11."
 print(phrase)
 
-myList = {1, 2, 3, 'a', 'b', 'c', 1.1, 2.2, 3.3}
+myList = [1, 2, 3, 'a', 'b', 'c', 1.1, 2.2, 3.3]
 	
 for value in myList:				#iterate through myList
 	if (isinstance(value, int)):	#check if value is int
