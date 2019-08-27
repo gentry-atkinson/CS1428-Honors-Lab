99d9d71dbf94a0a46d1936240198e220f79f4664
Gentry <gma23@txstate.edu>
2019-08-27 11:59:10 -0500

    adding solution to lab0

---
+++ atkinson_lab0.cpp
@@ -0,0 +1,23 @@
+//Gentry Atkinson
+//CS1428 Fall 2019
+//Lab 0
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World." << endl;
+    cout << "1. yes" << endl;
+    cout << "2. to translate code" << endl;
+    cout << "3. a step by step guide" << endl;
+    cout << "4. floating point" << endl;
+    cout << "5. gcc" << endl;
+    cout << "6. nothing" << endl;
+    cout << "7. an input to a function" << endl;
+    cout << "8. an integer" << endl;
+    cout << "9. output" << endl;
+    cout << "10. unsupervised machine learning" << endl;
+
+    return 0;
+}
