8d46736b8b1129258b3ccf3b03e505bbcf9853fa
Gentry Atkinson <gma23@txstate.edu>
2019-10-21 19:16:45 -0500

    adding the cpp file

---
+++ yourName_lab8.cpp
@@ -0,0 +1,18 @@
+//Your Name
+//CS1428 Fall 2018
+//Lab 8
+
+/******************************************
+Your client is a small publishing firm called GenCo.
+The need an application that can process some simple
+information about material submitted from aspiring
+authors. The have some code from a previous project
+that can be re-used but need you to write the framework.
+******************************************/
+
+int main(){
+
+    return 0;
+}
+
+
