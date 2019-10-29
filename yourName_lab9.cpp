c55aa6501cee1b1a37314419662f88ea3b79fc46
Gentry Atkinson <gma23@txstate.edu>
2019-10-28 19:22:09 -0500

    filled in main func

--- yourName_lab9.cpp
+++ yourName_lab9.cpp
@@ -3,19 +3,54 @@
 //Lab 9
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 float getCaloriesForBreed(string);
 float getCaloriesForBreed(char);
+float getCaloriesForBreed(float, string);
 
-float remainingCalories(float);
-float remainingCalories(string);
+float remainingCalories(float, float);
+float remainingCalories(string, float);
+float remainingCalories(string, float, float);
 
-float isSafeToEat(string);
-float isSafeToEat(char);
+bool isSafeToEat(string);
+bool isSafeToEat(char);
+bool isSafeToEat(string, string);
 
 
 int main(){
+    float caloriesPerDay;
+    cout << "------------------Test UI 1-------------------" << endl;
+    cout << "Enter a breed to calculate calories/day: " << endl;
+    caloriesPerDay = getCaloriesForBreed("labrador");
+    cout << "Enter a meal: " << endl;
+    caloriesPerDay = remainingCalories("dry", caloriesPerDay) << endl;
+    cout << "Your pupper needs " << caloriesPerDay << " more calories today." << endl << endl;
 
+    cout << "------------------Test UI 2-------------------" << endl;
+    cout << "Enter a breed to calculate calories/day: " << endl;
+    cout << "A) labrador" << endl;
+    cout << "B) terrier" << endl;
+    cout << "C) retriever" << endl;
+    cout << "D) mastiff"
+    caloriesPerDay = getCaloriesForBreed('B');
+    cout << "Enter a meal size: " << endl;
+    caloriesPerDay = remainingCalories(200, caloriesPerDay) << endl;
+    cout << "Your pupper needs " << caloriesPerDay << " more calories today." << endl << endl;
+
+    cout << "------------------Test UI 3-------------------" << endl;
+    cout << "Enter a breed to calculate calories/day: " << endl;
+    caloriesPerDay = getCaloriesForBreed("corgie");
+
+    cout << "------------------Test UI 4-------------------" << endl;
+    cout << "Enter a breed to predicted calories/day: " << endl;
+    caloriesPerDay = getCaloriesForBreed("terrier");
+    cout << "Enter a meal and # of cups: " << endl;
+    caloriesPerDay = remainingCalories("wet", 2.0, caloriesPerDay) << endl;
+    cout << "Your pupper needs " << caloriesPerDay << " more calories today." << endl << endl;
+
+    cout << "Thank you for using DogDiet." << endl;
+    return 0;
 }
