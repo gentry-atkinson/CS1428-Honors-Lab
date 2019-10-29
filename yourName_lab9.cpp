4145913eab6e19829eaba4e275f4e61df62d98ee
Gentry Atkinson <gma23@txstate.edu>
2019-10-28 19:06:52 -0500

    starting provided file for 9

---
+++ yourName_lab9.cpp
@@ -0,0 +1,21 @@
+//Your Name
+//CS1428 Fall 2019
+//Lab 9
+
+#include<iostream>
+
+using namespace std;
+
+float getCaloriesForBreed(string);
+float getCaloriesForBreed(char);
+
+float remainingCalories(float);
+float remainingCalories(string);
+
+float isSafeToEat(string);
+float isSafeToEat(char);
+
+
+int main(){
+
+}
